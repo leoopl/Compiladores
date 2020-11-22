2736bd143ea9941028500f7a85073390b7122727
Gabriel Cerqueira <gfc@ic.ufal.br>
2020-11-22 11:50:31 -0300

    att sin

--- sin.py
+++ sin.py
@@ -1,53 +1,116 @@
-import tabela_m as tabela
-op = {">": "rel", "<": "rel",
-      "=": "rel",  "+": "op_ad",\
-      "-": "op_ad", "*": "op_mul",\
-      "/": "op_mul", "<=": "op_rel",\
-      ">=": "op_rel", "<>": "op_rel",\
-      ":=": "op_atrib"}#op organizado
-from lex import erro#importado o metodo erro para uso na função analisador_sintatico()
+import tabela_m as tabela_m
 
-listaTokens = []
-pilhaNT = []
+#from lex import listaDeTokens
+from stack import Stack
+
+print(tabela_m.get("mais_fatores", "op_mul"))
+pilha_nt = Stack()
+pilha_nt.push("$")
+pilha_nt.push("programa")
+listaDeTokens = [["int", "ident", "reserv", ""], ["x", "ident", "id_var", ""],
+                 [",", "simb", "", ""], ["y", "ident",
+                                         "id_var", ""], [";", "simb", "", ""],
+                 ["begin", "ident", "reserv", ""], [
+                     "read", "ident", "reserv", ""],
+                 ["x", "ident", "id_var", ""], [";", "simb", "", ""], [
+                     "read", "ident", "reserv", ""],
+                 ["y", "ident", "id_var", ""], [";", "simb", "", ""],
+                 ["x", "ident", "id_var", ""], [":=", "op", "op_atrib", ""],
+                 ["y", "ident", "id_var", ""], ["*", "op", "op_mul", ""],
+                 ["x", "ident", "id_var", ""], [";", "simb", "", ""], ["end", "ident", "reserv", ""]]
+listaDeTokens.append("$")
 
 
-def push(data):
+def isTerminal(item):
+    if type(item) == list:
+        return True
 
-    while not data.empty():
-        tipoToken = "nt"
+    return (item == "op_ad" or item == "op_mul" or item == "op_atrib" or item == "id_var" or item == "id_func" or item == "id_proc" or item == "num" or item == "$" or item == "tipo_var")
 
-        if type(data[0]) == list:
-            tipoToken = "t"
-        #não existe o método .empty pra checar se a lista é vazia, substituido por if list == []
-        elif (data[0] == "op_ad" | data[0] == "op_mul" | data[0] == "op_rel" | data[0] == "op_atrib" |\
-        data[0] == "id_var" | data[0] == "id_proc" | data[0] == "id_func" | data[0] == "num" | data[0] == "$"):
-            tipoToken = "t"
 
-        pilhaNT.append([data.pop(), tipoToken])
+def erro():
+    print("erro sintatico")
+    exit()
 
-def compare(pilha, token):
-    if pilha == token:
-        pilhaNT.pop()
-        return listaTokens.pop()
 
-    aux = tabela.get(pilha, token)
-    if aux:
-        if aux  == "$":
-            return pilhaNT.pop()
-     
-def analisador_sintatico():
-    #não existe o método .push em lista, substituido por .append
-    pilhaNT.append(["$"])
-    pilhaNT.append(["programa"])
+def addTerminal(item):
+    for x in reversed(item):
+        pilha_nt.push(x)
+
+
+def getItemKey(item):
+
+    # para simbolos terminais e vazio($)
+    if type(item) == str:
+        return item
+
+    # para simbolos terminais
+
+    if item[1] == "op":
+        return item[2]
+
+    elif item[1] == "ident":
+        if item[0] == "int":
+            return "tipo_var"
+        return item[0] if item[2] == "reserv" else item[2]
+
+    if item[1] == "simb":
+        return item[0]
+
+    else:
+        return item[1]
+
+
+def compare(head, token, item_pilha):
+
+    if head == token == "$":
+        return "fim"
+
+    if isTerminal(item_pilha):
+        if head == token:
+            return "token-valido"
+
+        if head == "$":
+            return "$"
+
+        return "erro"
+
+    valorTabela = tabela_m.get(head, token)
+
+    return "erro" if not valorTabela else "terminal"
+
+
+def tokenValido():
+    pilha_nt.pop()
+    listaDeTokens.pop(0)
+
+
+def sin():
     fim = False
     while not fim:
-        head = pilhaNT[0]#não exite o método .top, substituido por [0]
+        head = getItemKey(pilha_nt.peek())
+        token = getItemKey(listaDeTokens[0])
+        print(head, token)
+        res_compare = compare(head, token, pilha_nt.peek())
+        print(res_compare)
+        if res_compare == "fim":
+            tokenValido()
+            fim = True
 
-        if pilhaNT == [] and not listaTokens == [] or not pilhaNT== [] and listaTokens== []:
+        elif res_compare == "token-valido":
+            tokenValido()
+
+        elif res_compare == "terminal":
+            if tabela_m.get(head, token)[0] != "$":
+                pilha_nt.pop()
+            addTerminal(tabela_m.get(head, token))
+
+        elif res_compare == "$":
+            pilha_nt.pop()
+            pilha_nt.pop()
+
+        elif res_compare == "erro":
             erro()
-            exit()
 
-        if head[1] == "t":
-            if type(head[0]) == list:
-                if head[0][1] == "op":
-                    break
+
+sin()
