410784c74fc1d59dea6b2de4a27c1e5bd97e04ab
Victor Sotero <victorvcdb@gmail.com>
2021-01-12 08:14:29 -0300

    controle de erro

--- models/tabela_sintatica.py
+++ models/tabela_sintatica.py
@@ -1,9 +1,19 @@
-class TSintatica:
+
+
+class Singleton:
+    _instance = None
+    tabela = None
 
     def __init__(self):
         self.tabela = {}
 
-    def addLexema(self, lex, cat):
+    @classmethod
+    def instance(cls):
+        if cls._instance is None:
+            cls._instance = cls()
+        return cls._instance
+
+    def addLexema(self, lex, cat, fp=None):
         aux = self.tabela.get(lex[0], False)
 
         if aux == False:
@@ -13,11 +23,11 @@ class TSintatica:
                 "value": "",
                 "escopo": "global"
             }
+            if cat == "id_func" or cat == "id_proc":
+                self.tabela[lex[0]]["inicialPosition"] = fp
+
             return self.tabela.get(lex[0])
         return False
 
     def check(self, lex):
-        return self.tabela.get(lex, False)  
-
-
-   
+        return self.tabela.get(lex, False)
